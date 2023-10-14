b6f23ff79771a892ba19f33a52b8d08e39e104fa
goatrick <thondapuritvik@gmail.com>
2023-10-14 14:41:36 +0530

    ssl certificates pushed

--- app.js
+++ app.js
@@ -1,29 +1,93 @@
-import express from "express";
-const app = express();
+import http from "http";
+import https from "https";
 
-//Import DB Connection File
-import "./utils/dbConnect.js";
+import fs from "fs";
 
-const port = process.env.PORT || 8080;
-
-//Import Routers
-import tasksRouter from "./routes/tasksRouter.js";
-import adminRouter from "./routes/adminRouter.js";
-
-app.use(express.json()); //json bodyparser
+//Import the router
+import { HTTPServer, HTTPSServer } from "./controllers/index.js";
+const httpPort = process.env.PORT || 8080;
+const httpsPort = 443;
 
 
-//It will serve views/index.html at / 
-app.use(express.static("views"));   //HomeRouter
-app.use("/api/tasks", tasksRouter); //TasksRouter
-app.use("/admin", adminRouter); //AdminRouter
-app.use("*", (req, res) => {
-    res.status(404).json({ error: "Route Not Found" })
+const SSLOptions = {
+    key: fs.readFileSync("./utils/ssl/private-key.pem"),
+    cert: fs.readFileSync("./utils/ssl/certificate.pem")
+}
+
+const httpServer = http.createServer(HTTPServer);
+const httpsServer = https.createServer(SSLOptions, HTTPSServer);
+
+
+httpServer.listen(httpPort, () => {
+    console.log(`HTTP Server started at ${httpPort}`);
 });
 
-app.listen(port, () => {
-    console.log(`Server Started at ${port}`);
+httpsServer.listen(httpsPort, () => {
+    console.log(`HTTPS Server started at ${httpsPort}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// import https from "https";
+// import http from "http";
+// import fs from "fs";
+
+// const app = express();
+// const httpPort = 8080;
+// const httpsPort = 443;
+
+// const SSLOptions = {
+//     key: fs.readFileSync("utils/ssl/private-key.pem"),
+//     cert: fs.readFileSync("utils/ssl/certificate.pem")
+// };
+
+// // Import Routers
+// import tasksRouter from "./routes/tasksRouter.js";
+// import adminRouter from "./routes/adminRouter.js";
+
+// app.use(express.json());
+
+// // It will serve views/index.html at /
+// app.use(express.static("views"));
+
+// app.use("/api/tasks", tasksRouter);
+// app.use("/admin", adminRouter);
+
+// // Add a middleware to redirect HTTP to HTTPS
+// app.use((req, res, next) => {
+//   if (req.protocol === 'http') {
+//     const httpsUrl = `https://${req.hostname}${req.url}`;
+//     return res.redirect(301, httpsUrl);
+//   }
+//   next();
+// });
+
+// app.use("*", (req, res) => {
+//   res.status(404).json({ error: "Route Not Found" });
+// });
+
+// const httpServer = http.createServer(app);
+// const httpsServer = https.createServer(SSLOptions, app);
+
+// httpServer.listen(httpPort, () => {
+//     console.log(`HTTP Server started at ${httpPort}`);
+// });
+
+// httpsServer.listen(httpsPort, () => {
+//     console.log(`HTTPS Server started at ${httpsPort}`);
+// });
+
+
 
 //there should be redirection to the 80 port number like we saw previously and that should occur in production
 
